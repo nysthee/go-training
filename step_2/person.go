fdb20371dd56ca13856a83d9f2b0cfc5ea077d51
Marc Grol <mgrol@xebia.com>
2015-05-29 19:46:03 +0200

    Added step 2

---
+++ step_2/person.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    // TODO: import standard libraries or 3rd party packages
+    "fmt"
+)
+
+// TODO: define your struct
+type Dummy struct {
+}
+
+func main() {
+    // TODO: create and populate the struct
+    d := Dummy{}
+
+    // TODO: print the struct and its attributes to stdout
+    fmt.Printf( "%+v", d )
+}
